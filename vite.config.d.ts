e158df24ed85a6ac93fd5c2f2e29c95ab12f048a
aliszd <70682709+aliszd@users.noreply.github.com>
2026-09-12 02:49:28 +0330

    db

--- vite.config.d.ts
+++ vite.config.d.ts
@@ -1,2 +1,9 @@
 declare const _default: any;
 export default _default;
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/persian-storybook/",
+  plugins: [react()],
+});
